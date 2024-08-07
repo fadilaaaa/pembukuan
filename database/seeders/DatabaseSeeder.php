49dedcb4d6cac81f556824a8007292a1276800e2
fadilaaaa <57476855+fadilaaaa@users.noreply.github.com>
2024-08-07 09:27:19 +0700

    chore: Update database seeder to generate daily cash transactions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,54 +32,54 @@ class DatabaseSeeder extends Seeder
             "password" => bcrypt("admin"),
             "role" => "admin",
         ]);
-        \App\Models\Kategori::create([
-            "nama" => "inventaris",
-            "class" => "badge-success"
-        ]);
-        \App\Models\Kategori::create([
-            "nama" => "personalia",
-            "class" => "badge-warning"
-        ]);
+        // \App\Models\Kategori::create([
+        //     "nama" => "inventaris",
+        //     "class" => "badge-success"
+        // ]);
+        // \App\Models\Kategori::create([
+        //     "nama" => "personalia",
+        //     "class" => "badge-warning"
+        // ]);
 
-        $datearr = CarbonPeriod::create(Carbon::now()->subYear()->startOfMonth(), Carbon::now())->toArray();
-        foreach ($datearr as $key => $date) {
-            if ($key == 0) {
-                $kas = new Kas();
-                $kas->tanggal = $date->translatedFormat('d F Y');
-                $kas->jenis = "masuk";
-                $kas->jumlah = 5_000_000;
-                $kas->saldo = 5_000_000;
-                $kas->keterangan = "Saldo Awal";
-                $kas->created_at = strtotime($date);
-                $kas->setNomorKas("/DK/" . $date->format('m') . "/" . $date->format('Y'));
-                // $kas->setSaldoAttribute();
-                $kas->save();
-            }
-            $kas = new Kas();
-            $kas->tanggal = $date->translatedFormat('d F Y');
-            $kas->jenis = 'masuk';
+        // $datearr = CarbonPeriod::create(Carbon::now()->subYear()->startOfMonth(), Carbon::now())->toArray();
+        // foreach ($datearr as $key => $date) {
+        //     if ($key == 0) {
+        //         $kas = new Kas();
+        //         $kas->tanggal = $date->translatedFormat('d F Y');
+        //         $kas->jenis = "masuk";
+        //         $kas->jumlah = 5_000_000;
+        //         $kas->saldo = 5_000_000;
+        //         $kas->keterangan = "Saldo Awal";
+        //         $kas->created_at = strtotime($date);
+        //         $kas->setNomorKas("/DK/" . $date->format('m') . "/" . $date->format('Y'));
+        //         // $kas->setSaldoAttribute();
+        //         $kas->save();
+        //     }
+        //     $kas = new Kas();
+        //     $kas->tanggal = $date->translatedFormat('d F Y');
+        //     $kas->jenis = 'masuk';
 
-            $kas->jumlah = $faker->numberBetween(100000, 1000000);
-            $kas->keterangan = $faker->sentence();
-            $kas->created_at = strtotime($date);
-            $kas->setSaldo();
-            $kategori = $faker->randomElement([1, 2]);
-            $kas->setNomorKas("/DK/" . $date->format('m') . "/" . $date->format('Y'));
-            $kas->save();
-            $kas->kategoris()->attach($kategori);
+        //     $kas->jumlah = $faker->numberBetween(100000, 1000000);
+        //     $kas->keterangan = $faker->sentence();
+        //     $kas->created_at = strtotime($date);
+        //     $kas->setSaldo();
+        //     $kategori = $faker->randomElement([1, 2]);
+        //     $kas->setNomorKas("/DK/" . $date->format('m') . "/" . $date->format('Y'));
+        //     $kas->save();
+        //     $kas->kategoris()->attach($kategori);
 
 
-            $kas = new Kas();
-            $kas->tanggal = $date->translatedFormat('d F Y');
-            $kas->jenis = 'keluar';
-            $kas->jumlah = $faker->numberBetween(100000, 1000000);
-            $kas->keterangan = $faker->sentence();
-            $kas->created_at = strtotime($date);
-            $kas->setSaldo();
-            $kategori = $faker->randomElement([1, 2]);
-            $kas->setNomorKas("/KK/" . $date->format('m') . "/" . $date->format('Y'));
-            $kas->save();
-            $kas->kategoris()->attach($kategori);
-        }
+        //     $kas = new Kas();
+        //     $kas->tanggal = $date->translatedFormat('d F Y');
+        //     $kas->jenis = 'keluar';
+        //     $kas->jumlah = $faker->numberBetween(100000, 1000000);
+        //     $kas->keterangan = $faker->sentence();
+        //     $kas->created_at = strtotime($date);
+        //     $kas->setSaldo();
+        //     $kategori = $faker->randomElement([1, 2]);
+        //     $kas->setNomorKas("/KK/" . $date->format('m') . "/" . $date->format('Y'));
+        //     $kas->save();
+        //     $kas->kategoris()->attach($kategori);
+        // }
     }
 }
